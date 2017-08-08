42ebb9a4386aaa4ead81644677c66a4048209f2c
liyangbin <liyanbin98@gmail.com>
2017-08-08 22:27:40 +0800

    fake_something

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/jquery-1" => "pages#jquery_1"
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
 
-  root "pages#jquery_1"
+  root "posts#index"
 end
