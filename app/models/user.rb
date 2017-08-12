65444cba96b2613a5504b5efe983d0106645b6b7
liyangbin <liyanbin98@gmail.com>
2017-08-12 22:13:16 +0800

    finish_raty

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,11 @@ class User < ApplicationRecord
     # # 取 email 的前半来显示，如果你也可以另开一个字段是 nickname 让用户可以自己编辑显示名称
     self.email.split("@").first
   end
-  
+
+  def is_admin?
+    role == "admin"
+  end
+
+  has_many :likes
+  has_many :liked_posts, :through => :likes, :source => :post
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   get "/jquery-3" => "pages#jquery_3"
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
-
+  resources :posts do
+    member do
+      post "like" => "posts#like"
+      post "unlike" => "posts#unlike"
+      post "toggle_flag" => "posts#toggle_flag"
+      post "rate" => "posts#rate"
+    end
+  end
   root "posts#index"
 end
